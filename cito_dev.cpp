efde2f7bb2682d64774928365db695abf6cb8457
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-11 18:22:52 +0900

    using calc by cparse

--- cito_dev.cpp
+++ cito_dev.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
+#include "cparse/shunting-yard.h"
 
 int main() {
 
-    std::cout << "Cito dev version 0.0.0.1" << std::endl;
+    std::cout << "Cito dev version 0.0.0.2" << std::endl;
 
     std::string cmd;
+
+    TokenMap vars;
     
     while(true) {
         std::cout << "cito > ";
         std::cin >> cmd;
 
-        std::cout << cmd << std::endl;
+        std::cout << calculator::calculate(cmd.c_str(), &vars)  << std::endl;
     }
 
     return 0;
